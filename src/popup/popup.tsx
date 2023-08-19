980de4637174c29638df3d4c9cecd87e46d1952c
Gerry Raditya <spectrevuln@gmail.com>
2023-08-19 22:30:10 +0700

    updated css loader

---
+++ src/popup/popup.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import './popup.css'
+
+
+
+const test = <img src="talentlytica.png" />
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+ReactDOM.createRoot(root).render(test)
